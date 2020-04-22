74ee3ce5dc562bc1273722661c8c997fb95ecf94
JonLiuFYI <2997583+JonLiuFYI@users.noreply.github.com>
2020-04-22 17:29:15 -0400

    bot skeleton (lots of holes to fill in)

--- clocc.py
+++ clocc.py
@@ -8,21 +8,28 @@ token = os.getenv('DISCORD_TOKEN')
 
 PREFIX = '⌚'
 
-
-class Clocc(commands.Bot):
-    def __init__(self):
-        super().__init__(command_prefix=PREFIX)
-
-    async def on_ready(self):
-        print(f'{self.user.name} has connected to Discord')
-
-    async def on_message(self, message):
-        if message.author == self.user.name:
-            return
-
-        if message.content == 'asdf':
-            await message.channel.send('ASDF indeed')
+clocc = commands.Bot(command_prefix=PREFIX,
+                     description='Convert your time to other time zones')
 
 
-bot = Clocc()
-bot.run(token)
+@clocc.event
+async def on_ready():
+    print(f'{clocc.user.name} connected to Discord')
+
+
+@clocc.command(name='time', help='Convert the given time')
+async def convert_time(ctx, t: str, *tzs):
+    await ctx.send(f'Time {t} in other time zones...')
+
+
+@clocc.command(name='mytimezone', help='Set your time zone')
+async def set_tz(ctx, t: str):
+    await ctx.send('Not implemented yet!')
+
+
+@clocc.command(name='target', help='Set what time zones this bot will convert to by default')
+async def set_target(ctx, *tzs):
+    await ctx.send('Not implemented yet!')
+
+
+clocc.run(token)
